a1e1376f6a23b5e35b99980c24b284e0b446928b
jonandereg <jonander.eg@gmail.com>
2023-03-05 10:48:31 +0100

    removed unused styles

---
+++ client/src/components/body.tsx
@@ -0,0 +1,11 @@
+import { Global } from '@emotion/react'
+
+export const GlobalBodyStyles = () => (
+  <Global
+    styles={`
+      body {
+        background-color: #000000;
+      }
+      `}
+  />
+)
--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,10 +1,11 @@
-import '@/styles/globals.css'
+import { GlobalBodyStyles } from '../components/body'
 import { ChakraProvider } from '@chakra-ui/react'
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider>
+      <GlobalBodyStyles />
       <Component {...pageProps} />
     </ChakraProvider>
   )
